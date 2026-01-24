7812e99f39e5d92002a39a873963b6fa167fc3d4
La-Tva <144448452+La-Tva@users.noreply.github.com>
2026-01-24 19:45:24 +0100

    Fix: Verify socket env var name change

--- frontend/src/controllers/canalsocketio.js
+++ frontend/src/controllers/canalsocketio.js
@@ -14,7 +14,7 @@ class CanalSocketio {
         this.nomDInstance = nom
 
         this.socket = io(
-            process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+            process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001",
             {
                 autoConnect: true,
                 reconnection: true,
